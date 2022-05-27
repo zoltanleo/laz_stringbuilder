d67e89c7a19da813f3f46f510122f4458d66f59b
ZoltanLeo <leobronstain@gmail.com>
2022-05-27 07:44:03 +0300

    1

---
+++ unit1.pas
@@ -0,0 +1,136 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, LCLType,
+  LazUTF8;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormShow(Sender: TObject);
+  private
+    FSL: TStringList;
+    FSB: TStringBuilder;
+  public
+
+  end;
+
+const
+{$IFDEF MSWINDOWS}
+fn = 'd:\Archive\code_source\stringbuilder\example_list.txt';
+{$ELSE}
+  {$IFDEF LINUX}
+  fn = 'd:\Archive\code_source\stringbuilder\example_list.txt';
+  {$ELSE}
+  fn = 'd:\Archive\code_source\stringbuilder\example_list.txt';
+  {$ENDIF}
+{$ENDIF}
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  FSB:= TStringBuilder.Create;
+  FSL:= TStringList.Create;
+  if FileExists(fn) then FSL.LoadFromFile(fn);
+  Memo1.ScrollBars:= ssAutoBoth;
+  Memo1.WordWrap:= False;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  FreeAndNil(FSL);
+  FreeAndNil(FSB);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  i: Integer;
+begin
+  FSB.Clear;
+
+  for i:= 0 to Pred(FSL.Count) do
+    FSB.Append(Trim(FSL.Strings[i]) + '~');
+
+  Memo1.Clear;
+  Memo1.Text:= FSB.ToString;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  i: Integer;
+begin
+  FSB.Clear;
+
+  for i:= 0 to Pred(FSL.Count) do
+    FSB.Append(Trim(FSL.Strings[i]) + '~').AppendLine('*');
+
+  Memo1.Clear;
+  Memo1.Text:= FSB.ToString;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  i: Integer;
+begin
+  FSB.Clear;
+
+  for i:= 0 to Pred(FSL.Count) do
+    FSB.AppendFormat('%d. %s(%s)%s ', [
+                      i + 1,
+                      '*',
+                      Trim(FSL.Strings[i]),
+                      '~'
+                      ]);
+
+  Memo1.Clear;
+  Memo1.Text:= FSB.ToString;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+const
+  ss = 'qrtwyeженксыф';
+var
+  ch_arr: array of AnsiChar;
+  i: Integer;
+begin
+  FSB.Clear;
+  SetLength(ch_arr,Length(ss));
+
+  for i:= 0 to Pred(Length(ss)) do
+    ch_arr[i]:= PAnsiChar(ss)[i];
+
+    FSB.Append(FSL.CommaText);
+
+  Memo1.Clear;
+
+  //for CounterVar := 0 to Pred(Count) do
+
+  FSB.CopyTo(10,ch_arr,2,15);
+end;
+
+end.
+
